b3d70a2d89bcf4614a28193d1a0f539bc43f7d05
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-10-19 22:29:09 -0700

    Added a test case with different classloaders to AccessPrivateField

--- src/com/gypsyengineer/innerclass/field/AccessPrivateField.java
+++ src/com/gypsyengineer/innerclass/field/AccessPrivateField.java
@@ -1,22 +1,62 @@
 package com.gypsyengineer.innerclass.field;
 
+import java.io.File;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class AccessPrivateField {
 
+    public static ClassLoader NO_PARENT_CLASSLOADER = null;
+
     public static void main(String[] args) throws Exception {
+        String classpath = System.getProperty("java.class.path");
+        System.out.println("classpath = " + classpath);
+
+        // we assume that classpath contains only one element
+        String url = "file://" + (new File(classpath).getAbsolutePath()) + "/";
+
+        URL[] urls = new URL[] { new URL(url) };
+        URLClassLoader cl = new URLClassLoader(urls, NO_PARENT_CLASSLOADER);
+
+        // make sure that we run with security manager
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
 
-        go();
+        test01();
+        test02(cl);
     }
 
-    public static void go() throws Exception {
-        Outer t = new Outer();
-        Method m = Outer.class.getDeclaredMethod(
-                "access$002", Outer.class, int.class);
+    public static void test01() throws Exception {
+        System.out.println("Test #1: try to modify a private field with the same classloader");
+        System.out.println("         (oops is expected)");
+
+        Outer outer = new Outer();
+        go(outer);
+        outer.check();
+    }
+
+    public static void test02(ClassLoader cl) throws Exception {
+        System.out.println("Test #2: try to modify a private field with different classloader");
+        System.out.println("         (an exception is expected)");
+
+        Class clazz = cl.loadClass(
+            "com.gypsyengineer.innerclass.field.AccessPrivateField");
+
+        // make sure that we loaded new class
+        if (AccessPrivateField.class.equals(clazz)) {
+            throw new RuntimeException("Couldn't load different AccessPrivateField with new class loader");
+        }
+
+        Outer b = new Outer();
+        Method m = clazz.getDeclaredMethod("go", Object.class);
+        m.invoke(null, b);
+    }
+
+    public static void go(Object t) throws Exception {
+        Method m = t.getClass().getDeclaredMethod(
+                "access$002", t.getClass(), int.class);
         m.invoke(null, t, -1);
-        t.check();
     }
 }
