6fa4d57c0fa9326db54e49b4ba89dfc1e23241bc
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-09-20 17:59:22 +0200

    Added SchemaFactoryXXEWithURL

---
+++ src/main/java/com/gypsyengineer/java/xml/dtd/xxe/SchemaFactoryXXEWithURL.java
@@ -0,0 +1,169 @@
+package com.gypsyengineer.java.xml.dtd.xxe;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class SchemaFactoryXXEWithURL {
+
+    /*
+     * XML and XSD documents were taken from https://www.w3schools.com/xml/schema_example.asp
+     */
+    public static void main(String... args) throws Exception {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                + "\n"
+                + "<shiporder orderid=\"889923\"\n"
+                + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+                + "xsi:noNamespaceSchemaLocation=\"shiporder.xsd\">\n"
+                + "  <orderperson>John Smith</orderperson>\n"
+                + "  <shipto>\n"
+                + "    <name>Ola Nordmann</name>\n"
+                + "    <address>Langgt 23</address>\n"
+                + "    <city>4000 Stavanger</city>\n"
+                + "    <country>Norway</country>\n"
+                + "  </shipto>\n"
+                + "  <item>\n"
+                + "    <title>Empire Burlesque</title>\n"
+                + "    <note>Special Edition</note>\n"
+                + "    <quantity>1</quantity>\n"
+                + "    <price>10.90</price>\n"
+                + "  </item>\n"
+                + "  <item>\n"
+                + "    <title>Hide your heart</title>\n"
+                + "    <quantity>1</quantity>\n"
+                + "    <price>9.90</price>\n"
+                + "  </item>\n"
+                + "</shiporder>";
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document doc = documentBuilder.parse(new InputSource(new ByteArrayInputStream(
+                xml.getBytes(StandardCharsets.UTF_8))));
+
+        SimpleHttpServer server = new SimpleHttpServer("test");
+        try {
+            String xsd = String.format(
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
+                            + " <!DOCTYPE foo [  \n"
+                            + "   <!ELEMENT foo ANY >\n"
+                            + "   <!ENTITY xxe SYSTEM \"%s\" >\n"
+                            + "]>\n"
+                            + "<foo>&xxe;</foo>"
+                            + "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n"
+                            + "\n" + "<xs:element name=\"shiporder\">\n"
+                            + "  <xs:complexType>\n"
+                            + "    <xs:sequence>\n"
+                            + "      <xs:element name=\"orderperson\" type=\"xs:string\"/>\n"
+                            + "      <xs:element name=\"shipto\">\n"
+                            + "        <xs:complexType>\n"
+                            + "          <xs:sequence>\n"
+                            + "            <xs:element name=\"name\" type=\"xs:string\"/>\n"
+                            + "            <xs:element name=\"address\" type=\"xs:string\"/>\n"
+                            + "            <xs:element name=\"city\" type=\"xs:string\"/>\n"
+                            + "            <xs:element name=\"country\" type=\"xs:string\"/>\n"
+                            + "          </xs:sequence>\n"
+                            + "        </xs:complexType>\n"
+                            + "      </xs:element>\n"
+                            + "      <xs:element name=\"item\" maxOccurs=\"unbounded\">\n"
+                            + "        <xs:complexType>\n"
+                            + "          <xs:sequence>\n"
+                            + "            <xs:element name=\"title\" type=\"xs:string\"/>\n"
+                            + "            <xs:element name=\"note\" type=\"xs:string\" minOccurs=\"0\"/>\n"
+                            + "            <xs:element name=\"quantity\" type=\"xs:positiveInteger\"/>\n"
+                            + "            <xs:element name=\"price\" type=\"xs:decimal\"/>\n"
+                            + "          </xs:sequence>\n"
+                            + "        </xs:complexType>\n"
+                            + "      </xs:element>\n"
+                            + "    </xs:sequence>\n"
+                            + "    <xs:attribute name=\"orderid\" type=\"xs:string\" use=\"required\"/>\n"
+                            + "  </xs:complexType>\n"
+                            + "</xs:element>\n"
+                            + "\n"
+                            + "</xs:schema>", server.url());
+
+            String language = "http://www.w3.org/2001/XMLSchema";
+            SchemaFactory factory = SchemaFactory.newInstance(language);
+
+            /*
+             * Enabling XMLConstants.FEATURE_SECURE_PROCESSING fixes the issue:
+             *
+             *      factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING ,true);
+             */
+
+            Schema schema = factory.newSchema(new StreamSource(new ByteArrayInputStream(
+                    xsd.getBytes(StandardCharsets.UTF_8))));
+
+            Validator validator = schema.newValidator();
+            validator.validate(new DOMSource(doc));
+        } finally {
+            server.close();
+            if (server.accepted()) {
+                throw new RuntimeException("Oops! Accepted!");
+            }
+        }
+    }
+
+    private static class SimpleHttpServer implements HttpHandler, AutoCloseable {
+
+        private final String content;
+        private final HttpServer server;
+
+        private boolean flag = false;
+
+        SimpleHttpServer(String content) throws IOException {
+            server = HttpServer.create(new InetSocketAddress(0), 0);
+            server.createContext("/test", this);
+            server.setExecutor(null);
+            server.start();
+            this.content = content;
+        }
+
+        String url() {
+            return String.format("http://localhost:%d/test", server.getAddress().getPort());
+        }
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            System.out.println("server: accepted a request");
+            synchronized (this) {
+                flag = true;
+            }
+            byte[] response = content.getBytes();
+            t.sendResponseHeaders(200, response.length);
+            try (OutputStream os = t.getResponseBody()) {
+                os.write(response);
+            }
+        }
+
+        public boolean accepted() {
+            synchronized (this) {
+                return flag;
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            server.stop(0);
+        }
+    }
+}
